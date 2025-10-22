4ceac93a7833b4b704e22397e241f2a4a42e0bea
Juanco <juanffo874@gmail.com>
2025-10-22 00:57:30 -0600

    Instala dependencias y agrega funciones para la conexion con la base de datos

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv"
+dotenv.config()
+
+export const db = new Sequelize(process.env.DATABASE_URL as string, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        },
+    }
+})
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express from 'express' 
-import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log("Conexion exitosa a la base de datos")
+    } catch (error) {
+        console.log(error)
+    }
+}
+connectDB()
 
 const app = express()
 
 app.use(morgan('dev'))
 
 app.use(express.json())
-
 
 
 export default app
