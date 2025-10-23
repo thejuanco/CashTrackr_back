04e0042301ab9096d4ffe0582085eb9613885a81
Juanco <juanffo874@gmail.com>
2025-10-22 23:17:40 -0600

    Crea el modelo de presupuesto

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const db = new Sequelize(process.env.DATABASE_URL as string, {
+    models: [__dirname + '/../models/**/*'],
     dialect: "postgres",
+    logging: false,
     dialectOptions: {
         ssl: {
             require: false,
