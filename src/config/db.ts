3e4c52e827f2ef22f8f900d76ce373c410788216
Juanco <juanffo874@gmail.com>
2025-10-22 23:05:10 -0600

    Actualiza la conexion con la base de datos

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,8 +6,8 @@ export const db = new Sequelize(process.env.DATABASE_URL as string, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false
+            require: false,
+            rejectUnauthorized: false,
         },
     }
 })
