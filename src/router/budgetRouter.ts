73eb0e27aab706c4909f19b088fd8d5dcf4aa77f
Juanco <juanffo874@gmail.com>
2025-10-25 23:14:56 -0600

    Agrega validaciones para crear los presupuestos

--- src/router/budgetRouter.ts
+++ src/router/budgetRouter.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
 router.get("/", BudgetController.getAll)
-router.post("/", BudgetController.create)
+
+router.post("/", 
+    body("name")
+        .notEmpty().withMessage("El nombre del presupuesto no puede ir vacío"),
+    body("amount")
+        .notEmpty().withMessage("La cantidad del presupuesto no puede ir vacío")  
+        .isNumeric().withMessage("Cantidad no válida") 
+        .custom(value => value > 0 ).withMessage("El presupuesto debe ser mayor a cero"),
+    handleInputErrors,
+BudgetController.create)
+
 router.get("/:id", BudgetController.getById)
 router.put("/:id", BudgetController.updateById)
 router.delete("/:id", BudgetController.deleteById)
