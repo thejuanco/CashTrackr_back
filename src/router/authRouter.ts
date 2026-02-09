0285c94c67eac16739a8259e4de24706df246fa3
Juanco <juanffo874@gmail.com>
2026-02-09 01:53:11 -0600

    feat: Agrega la validación para crear cuenta de usuarios

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { BudgetController } from "../controllers/authController";
 
 const router = Router()
 
-router.post('/create-account', BudgetController.createAccount)
+router.post('/create-account', 
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacío'),
+    body('password')
+        .isLength({min: 8}).withMessage('La contraseña es muy corta'),
+    body('email')
+        .isEmail().withMessage('El correo electronico no es valído'),
+BudgetController.createAccount)
 
 export default router
