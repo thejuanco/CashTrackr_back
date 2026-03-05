3decb15afb93212714337e9c91d700f547bf707c
Juanco <juanffo874@gmail.com>
2026-03-04 23:26:46 -0600

    feat: Valida que el usuario no este duplicado

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,8 +4,19 @@ import User from "../models/User"
 export class BudgetController {
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const {email} = req.body
+
+            //prevenir usuarios duplicados
+            const userExists = await User.findOne({where: {email}})
+            if(userExists){
+                const error = new Error('El usuario ya existe')
+                return res.status(409).json({error: error.message})
+            }
+
+            //Guardar el usuario
             const user = new User(req.body)
             await user.save()
+
             res.json('Cuenta creada correctamente')
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
