51c6a0e193f93267c35c93a6be4bcfe38b1fbe2f
Juanco <juanffo874@gmail.com>
2026-03-04 23:22:01 -0600

    feat: Añade el codigo para registrar un usuario

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,14 @@
 import { type Request, type Response } from "express"
+import User from "../models/User"
 
 export class BudgetController {
     static createAccount = async (req: Request, res: Response) => {
-        res.json({ message: "Cuenta creada exitosamente" })
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Cuenta creada correctamente')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
     }
 }
